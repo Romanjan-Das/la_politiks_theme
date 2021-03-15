8f0d0e79de2fb00186d2a24e82cb6c884c5635d4
Romanjan-Das <romanjandas.mail@gmail.com>
2021-03-15 21:31:27 +0530

    added few styles for portrait mode

--- single.php
+++ single.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 <!--------------this is the template for single blog posts----------->
 
-    <div class="container single-post">
-        <?php if(has_post_thumbnail()):?><img class="single-post-image" src="<?php the_post_thumbnail_url('smallest');?>"><?php endif;?>
+    <div class="container single-postt">
+        <?php if(has_post_thumbnail()):?><img class="single-postt-image" src="<?php the_post_thumbnail_url('smallest');?>"><?php endif;?>
         <?php if(have_posts()):while(have_posts()):the_post();?><h1><?php the_title();?></h1><?php the_content();endwhile;endif; ?>
     </div>
 
