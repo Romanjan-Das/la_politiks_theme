80255c8a15d8bd6c043b768ac6f58984a1fe7a52
Romanjan-Das <romanjandas.mail@gmail.com>
2021-03-21 14:53:19 +0530

    added comment facility

--- single.php
+++ single.php
@@ -4,6 +4,12 @@
     <div class="container single-postt">
         <?php if(has_post_thumbnail()):?><img class="single-postt-image" src="<?php the_post_thumbnail_url('smallest');?>"><?php endif;?>
         <?php if(have_posts()):while(have_posts()):the_post();?><h1><?php the_title();?></h1><?php the_content();endwhile;endif; ?>
+        <?php
+        // If comments are open or we have at least one comment, load up the comment template.
+        if ( comments_open() || get_comments_number() ) :
+            comments_template();
+        endif;
+        ?>
     </div>
 
 <?php get_footer(); ?>
