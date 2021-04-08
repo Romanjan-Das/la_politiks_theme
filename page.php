9cd3cb3effc0f64fe454813eba48ba887fe29ba9
romanjan-das <romanjandas.mail@gmail.com>
2021-04-09 00:09:40 +0530

    fixed individual page title and styling

--- page.php
+++ page.php
@@ -2,7 +2,7 @@
 <!--------------this is the template for pages ie linked pages in navigation bar----------->
 
     <div class="container individual-page">
-        <?php if(have_posts()):while(have_posts()):the_post();the_title();the_content();endwhile;endif; ?>
+        <?php if(have_posts()):while(have_posts()):the_post();?><div class="individual-page-title"><?php the_title();?></div><?php the_content();endwhile;endif; ?>
     </div>
 
 <?php get_footer(); ?>
