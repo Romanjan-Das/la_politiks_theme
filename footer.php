469f169c0454f048ad9883f671b13e950db694ca
romanjan-das <romanjandas.mail@gmail.com>
2021-04-12 01:21:24 +0530

    improved portrait mode styling and added footer content

--- footer.php
+++ footer.php
@@ -1,4 +1,6 @@
 <?php wp_footer(); ?>
-<div id="colophon"></div>
+<div id="colophon">
+    <div style="color:white;padding:4rem">&#169; All rights reserved by Republika,ARSD Political Dept.</div>
+</div>
 </body>
 </html>
